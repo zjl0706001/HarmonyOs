21724556713acc75055846e688928cc353fad47b
zjl <2644724435@qq.com>
2025-02-18 09:30:05 +0800

    1

--- 作业/王者荣耀/index.js
+++ 作业/王者荣耀/index.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function scrollToSection(index) {
     sections[index].scrollIntoView({ behavior: "smooth" });
   }
+
   window.addEventListener("wheel", (event) => {
     if (event.deltaY > 0) {
       if (currentSection < totalSections - 1) {
@@ -33,3 +34,76 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+//page1轮播图
+const lunbo = document.querySelectorAll("#page1 .lunboimg");
+const prev = document.querySelector("#page1 .jiantou.left");
+const next = document.querySelector("#page1 .jiantou.right");
+const radius = document.querySelector("#page1 .radius");
+let newid = 0;
+let timerId;
+
+function radiusxianshi(id, k) {
+  if (k) radius.children[id].className = "";
+  else radius.children[id].className = "radius-active";
+}
+
+function createLi() {
+  for (
+    let i = 0;
+    i < document.querySelectorAll("#page1 .lunboimg").length;
+    i++
+  ) {
+    let li = document.createElement("li");
+    radius.appendChild(li);
+  }
+  radius.children[0].className = "radius-active";
+}
+
+createLi();
+
+const startLoop = () => {
+  clearInterval(timerId);
+  timerId = setInterval(() => {
+    radiusxianshi(newid % 5, 1);
+    lunbo[newid++ % 5].style.opacity = "0";
+    lunbo[newid % 5].style.opacity = "1";
+    radiusxianshi(newid % 5, 0);
+  }, 10000);
+};
+
+startLoop();
+
+prev.onclick = () => {
+  clearInterval(timerId);
+  radiusxianshi(newid % 5, 1);
+  lunbo[newid % 5].style.opacity = "0";
+  newid = newid === 0 ? 4 : newid - 1;
+  lunbo[newid % 5].style.opacity = "1";
+  radiusxianshi(newid % 5, 0);
+  startLoop();
+};
+
+next.onclick = () => {
+  clearInterval(timerId);
+  radiusxianshi(newid % 5, 1);
+  lunbo[newid++ % 5].style.opacity = "0";
+  lunbo[newid % 5].style.opacity = "1";
+  radiusxianshi(newid % 5, 0);
+  startLoop();
+};
+
+function cirMouse() {
+  for (let i = 0; i < radius.children.length; i++) {
+    radius.children[i].onmouseover = function () {
+      clearInterval(timerId);
+      radiusxianshi(newid % 5, 1);
+      lunbo[newid % 5].style.opacity = "0";
+      newid = i;
+      lunbo[newid].style.opacity = "1";
+      radiusxianshi(i, 0);
+      startLoop();
+    };
+  }
+}
+
+cirMouse();
